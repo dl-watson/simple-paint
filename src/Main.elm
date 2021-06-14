8226e7061aa6c90cf418ab912e1bcc7cd6ab27eb
dl-watson <dlarkinwatson@gmail.com>
2021-06-14 12:47:34 -0700

    adding next steps

--- src/Main.elm
+++ src/Main.elm
@@ -7,11 +7,11 @@ import Canvas exposing (..)
 import Canvas.Settings exposing (..)
 import Canvas.Settings.Line exposing (..)
 import Color
-import Debug exposing (log)
-import Html exposing (Html, aside, div, h1, img, text)
-import Html.Attributes exposing (src, style)
+import Html exposing (..)
+import Html.Attributes exposing (class, src, style)
 import Html.Events.Extra.Mouse as Mouse
-import Html.Attributes exposing (class)
+
+
 
 {-
    elm-canvas:
@@ -105,7 +105,7 @@ update msg model =
                     point :: prevStroke
             in
             if model.isDrawing then
-                { model | strokes = currentStroke :: ( List.tail model.strokes |> Maybe.withDefault model.strokes ) }
+                { model | strokes = currentStroke :: (List.tail model.strokes |> Maybe.withDefault model.strokes) }
 
             else
                 model
@@ -122,24 +122,23 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "container"]
-        [ Canvas.toHtml ( width, height )
+    div [ class "container" ]
+        [ p [ class "title" ] [ Html.text "Draw something!" ]
+        , Canvas.toHtml ( width, height )
             [ Mouse.onDown (.offsetPos >> CanvasMouseDown)
             , Mouse.onMove (.offsetPos >> CanvasMouseMove)
             , Mouse.onUp (\_ -> CanvasMouseUp)
             ]
             -- this first shape fills the canvas with white
             [ shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
-            -- this second shape dictates how each Mouse.onDown stroke is drawn
-            , shapes [ stroke Color.black, lineCap RoundCap, lineWidth 2, lineJoin RoundJoin ] (List.map createPath model.strokes) ]
+
+            -- this second shape dictates how each Mouse.onDown line stroke is drawn
+            , shapes [ stroke Color.black, lineCap RoundCap, lineWidth 2, lineJoin RoundJoin ] (List.map createPath model.strokes)
+            ]
+        , div
+            []
+            [ Html.text "color" ]
         ]
-
-
-
--- we have N lines to draw, but all we need to know is how to draw ONE line between two Points, then iterate
--- so we have our list of strokes, for each one of them, we'll create a SHAPE using shapes
--- 2: for each stroke, we split off the starting point, from all the others
--- the first point goes inside `path startingPoint`, the rest we accumulate in a list of `lineTo segments`
 
 
 {-| createPath programmatically generates a path with a starting point and a list of segments, where the starting point is the first element in the model's strokes list, and the list of segments are comprised from the rest.
@@ -150,27 +149,21 @@ createPath stroke =
         startingPoint =
             List.head (List.reverse stroke) |> Maybe.withDefault ( 0, 0 )
 
-        -- assume this won't fail, if it does, it will be pretty obvious
         segments =
             List.tail (List.reverse stroke) |> Maybe.withDefault []
     in
-    -- List Point -> List PathSegment
-    -- lineTo : Point -> PathSegment
     path startingPoint (List.map (\segment -> lineTo segment) segments)
 
 
 
---                  List.map(segments, segment => lineTo(segment))
--- line from (x1, y1) to (x2, y2)
--- Mouse.onDown -> lineWidth, (lineCap = "round"), lineTo, stroke
--- initialPoint = clientX / clientY
--- record the mouse positions between clicking and releasing
--- accumulate a list of those mouse positions and add those to another list of all the strokes
--- gives us undo/redo if we want
--- gives us the ability to completely separate event logic from data
--- hacks to cut down on size of list for very long-running drawings (save the pixel-buffer of the canvas and load it, for example, letting us delete the list but losing undo/redo for it)
--- NOTES:
--- with our current design, a single click doesn't color a single pixel
+{-
+   NEXT:
+       * implement undo/redo
+       * implement clear all
+       * implement draw point
+       * implement color picker
+       * implement line size picker
+-}
 ---- PROGRAM ----
 
 
